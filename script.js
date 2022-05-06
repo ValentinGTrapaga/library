faf1670a47d53dd93258acde3d79911deaa80292
ValentinGTrapaga <gonzaleztrapagav@gmail.com>
2022-05-06 05:31:13 -0300

    functionality ok - needs style

--- script.js
+++ script.js
@@ -22,6 +22,13 @@ let myLibrary = [
 const submit = document.getElementById('submit-btn')
 const formInputs = document.getElementById('book-form').elements
 const table = document.getElementById('table')
+const inputTitle = document.getElementById('title')
+const inputAuthor = document.getElementById('author')
+const inputPages = document.getElementById('pages')
+const inputRead = document.getElementById('read')
+let delBtns = document.querySelectorAll(".del-btn")
+
+console.log(delBtns)
 
 function Book(title, author, pages, read = false) {
     this.title = title
@@ -30,27 +37,85 @@ function Book(title, author, pages, read = false) {
     this.read = read
 }
 
-function checkLibrary() {
+function addBook() {
+    let bookToAdd = new Book(inputTitle.value, inputAuthor.value, parseInt(inputPages.value), inputRead.checked)
+    myLibrary.push(bookToAdd)
+}
+
+function loadLibrary() {
     let bookTitle = ""
     let bookAuthor = ""
     let bookPages = 0
     let bookRead = false
+    let textToInject = ""
 
     for (let i = 0; i < myLibrary.length; i++) {
-        bookTitle = myLibrary[i][title]
-        bookAuthor = myLibrary[i][author]
-        bookPages = myLibrary[i][title]
-        bookRead = myLibrary[i][read]
+        bookTitle = myLibrary[i]["title"]
+        bookAuthor = myLibrary[i]["author"]
+        bookPages = myLibrary[i]["pages"]
+        bookRead = myLibrary[i]["read"]
+
+        textToInject +=
+            `<tbody>
+                <tr class="bookRow">
+                    <td>${bookTitle}</td>
+                    <td>${bookAuthor}</td>
+                    <td>${bookPages}</td>
+                    <td>${bookRead ? "Yes" : "No"}</td>
+                </tr>
+            </tbody>`
     }
-    console.log(bookTitle, bookAuthor, bookPages, bookRead)
+    table.innerHTML = `
+        <table id="table">
+            <tr id="table-head">
+                <th>Title</th>
+                <th>Author</th>
+                <th>Pages</th>
+                <th>Read</th>
+                <th>Delete</th>
+            </tr>
+        </table>`
+    table.innerHTML += textToInject
+
+    delBtns = document.querySelectorAll(".del-btn")
 }
 
-checkLibrary()
+function deleteRow(ind) {
+    myLibrary.splice(ind, 1)
+    loadLibrary()
+    appendDelBtns()
+}
+
+function appendDelBtns() {
+    let bookRows = document.querySelectorAll(".bookRow")
+    bookRows.forEach((row, index) => {
+        const btn = document.createElement("button")
+        btn.classList = "del-btn"
+        btn.id = index
+        btn.innerText = "Delete"
+        row.appendChild(btn)
+    })
+
+    delBtns = document.querySelectorAll(".del-btn")
+    console.log(delBtns)
+    delBtns.forEach((btn) => {
+        btn.addEventListener('click', (event) => {
+            console.log(event.target.id)
+            deleteRow(event.target.id)
+        })
+    })
+}
+
+loadLibrary()
+appendDelBtns()
 
 submit.addEventListener('click', () => {
-    checkLibrary()
+    addBook()
+    loadLibrary()
+    console.table(myLibrary)
+    appendDelBtns() //Anda bien la funcion
 })
 
-/* function addBook() {
 
-} */
+
+
